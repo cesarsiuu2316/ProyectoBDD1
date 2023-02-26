b7f9f33919ccdb9fb4e896c4501e155af744e811
cesarsiuu2316 <cesar23augusto16@gmail.com>
2023-02-25 18:39:05 -0600

    first commit

---
+++ src/proyectobd1/MariaDBConnection.java
@@ -0,0 +1,42 @@
+package proyectobd1;
+
+import org.mariadb.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.mariadb.jdbc.Statement;
+import java.sql.ResultSet;
+
+public class MariaDBConnection {
+    Connection connection = null;
+    private String url = "jdbc:mariadb://localhost:3306/northwind";
+    private String user = "root";
+    private String pwd = "cesar2316";
+    
+    MariaDBConnection(){        
+        try {
+            connection = (Connection) DriverManager.getConnection(url, user, pwd);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Successfully connected to database.");
+    }    
+    
+    public void proyeccion(String tabla, String columna1, String columna2){
+        //MariaDBConnection c = new MariaDBConnection();
+        Statement st = null;
+        ResultSet rs = null;
+        String selectfrom = "select * from " + tabla;
+        try {
+            st = connection.createStatement();
+            rs = st.executeQuery(selectfrom);
+            System.out.println("-----------------");
+            while(rs.next()){
+                System.out.println(rs.getString(columna1) + " " + rs.getString(columna2));
+            }
+            System.out.println("-----------------");
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }    
+    }
+}
